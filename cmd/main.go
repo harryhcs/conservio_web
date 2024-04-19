97ffc643c46f920ce51651a2a1673f605624015b
Herman Stander <her.stander@gmail.com>
2024-04-19 21:20:45 +0200

    add env file

--- cmd/main.go
+++ cmd/main.go
@@ -1,17 +1,29 @@
 package main
 
 import (
+	"log"
+
 	"github.com/harryhcs/conservio_web/handler"
 	"github.com/hasura/go-graphql-client"
+	"github.com/joho/godotenv"
 	"github.com/labstack/echo/v4"
 )
 
 func main() {
 
+	var envs map[string]string
+	envs, err := godotenv.Read(".env")
+
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	api := envs["GQL_API"]
+
 	app := echo.New()
 	app.Static("/static", "static")
 
-	client := graphql.NewClient("https://staging.graph.conservio.com/graphql", nil)
+	client := graphql.NewClient(api, nil)
 
 	paymentStatusHandler := handler.PayStatusHandler{
 		Client: client,
--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/a-h/templ v0.2.663 // indirect
 	github.com/google/uuid v1.6.0 // indirect
 	github.com/hasura/go-graphql-client v0.12.1 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
 	github.com/labstack/echo/v4 v4.12.0 // indirect
 	github.com/labstack/gommon v0.4.2 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
